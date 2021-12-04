8eb406475ddb0750ceae6b7fd7935bba2f46a6be
constan1000 <hrairimejdi99@gmail.com>
2021-12-04 21:16:47 +0100

    Print array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+        }
+        else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        int[] tab = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            tab[i] = i;
+            if (i == size - 1)
+            {
+                Console.WriteLine(tab[i]);
+            }
+            else
+            {
+                Console.Write(tab[i] + " ");
+            }
+        }
+        return (tab);
+    }
+}
