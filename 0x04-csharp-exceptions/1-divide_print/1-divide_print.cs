1583219a2ed1ae9e844dd3907aa38af4df237a1e
constan1000 <hrairimejdi99@gmail.com>
2021-12-19 20:54:18 +0100

    lateeeeeeee

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            int i = 0;
+            try
+            {
+                i = a / b;
+            }
+            catch (DivideByZeroException)
+            {
+                i = 0;
+                Console.WriteLine("Cannot divide by zero");
+            }
+            finally
+            {
+                Console.WriteLine("{0} / {1} = {2}", a, b, i);
+            }
+        }
+    }
---
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+        {
+            List<int> n = new List<int>();
+
+            for (int i = 0; i < listLength; i++)
+            {
+                try{
+                    n.Add(list1[i] / list2[i]);
+                }
+                catch (DivideByZeroException){
+                    Console.WriteLine("Cannot divide by zero");
+                    n.Add(0);
+                }
+                catch (ArgumentOutOfRangeException){
+                    Console.WriteLine("Out of range");
+                }
+            }
+            return(n);
+        }
+    }
