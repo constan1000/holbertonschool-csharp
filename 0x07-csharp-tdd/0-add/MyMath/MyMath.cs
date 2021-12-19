ae9756140fd957966df6c7d7f421aac938ee2664
constan1000 <hrairimejdi99@gmail.com>
2021-12-19 20:55:34 +0100

    again

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+/// <summary>
+/// Creating an operation class simple add ...
+/// </summary>
+namespace MyMath
+{
+    /// <summary>
+    /// the class of all operations
+    /// </summary>
+    public class Operations
+    {
+        /// the Add function
+        /// <param name="a">val a</param>
+        /// <param name="b"> val b</param>
+        /// <returns> a + b = int</returns>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
